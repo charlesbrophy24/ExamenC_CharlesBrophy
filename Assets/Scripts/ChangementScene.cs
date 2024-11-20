ee1615d9cfaf783f7a29dfad7108c8f101fcf56a
charlesbrophy24 <charlesbrophy05@gmail.com>
2024-11-20 15:05:22 -0500

    Changement de scene fonctionnel

---
+++ Assets/Scripts/ChangementScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ChangementScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void changementScene(){
+
+        SceneManager.LoadScene("ExamenC_02");
+
+   }
+
+
+}
